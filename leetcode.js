8ef691ead3d7b66eadb0417abcc19d5ef00e4981
Qulllaia <91777569+Qulllaia@users.noreply.github.com>
2024-09-27 01:09:41 +0300

    215

--- leetcode.js
+++ leetcode.js
@@ -62,9 +62,38 @@
 // console.log(findPeakGrid(mat));
 
 //70. Climbing Stairs
-var climbStairs = function (n) {
-  if (n <= 3) return n;
-  return climbStairs(n - 3) + 2 * climbStairs(n - 2);
-};
+// var climbStairs = function (n) {
+//   if (n <= 3) return n;
+//   return climbStairs(n - 3) + 2 * climbStairs(n - 2);
+// };
 
-console.log(climbStairs(5));
+// console.log(climbStairs(5));
+//215. Kth Largest Element in an Array
+var findKthLargest = function(nums, k) {
+  return radixSort(nums)[nums.length-k]
+};
+const getDigit = (num,place) =>{
+  if (num === 0) return 0
+  return Math.floor(Math.abs(num)/Math.pow(10,place))%10
+}
+
+const mostDigits = (nums) =>{
+  let mostDigit = 0
+  for(let i = 0; i < nums.length; i++){
+      mostDigit = Math.max(mostDigit, nums[i] === 0 ? 1 : Math.floor(Math.log10(Math.abs(nums[i])))+1)
+  }
+  return mostDigit
+}
+
+const radixSort = (nums) =>{
+  let mostDigit = mostDigits(nums)
+  for(let j = 0; j < mostDigit; j++){
+      let bucket = Array.from({length:20}, ()=>[])
+      for(let i = 0; i < nums.length; i++){
+          let digit = getDigit(nums[i],j)
+          bucket[nums[i] >= 0 ? 10 + digit : 10 - digit].push(nums[i])
+      }
+      nums = [].concat(...bucket)
+  }
+  return nums
+}
